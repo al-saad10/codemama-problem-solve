42940839fa0dfd51e4103f472d1def9412336a20
ERR0r 404 <xubayer001@gmail.com>
2024-09-12 22:26:32 +0600

    Create codmama_prblm3.js

---
+++ codmama_prblm3.js
@@ -0,0 +1,58 @@
+/*
+Description:
+ন্যূনতম তবু গুরুত্বপূর্ণ
+
+EN
+সমস্যার বিবরণ
+এমন একটি প্রোগ্রাম লিখ যেখানে ইউজারের লিখা তিনটি সংখ্যা থেকে সর্বনিম্ন সংখ্যাটি প্রিন্ট হবে।
+
+ইনপুট
+ইনপুট হবে তিনটি integer সংখ্যা।
+
+আউটপুট
+আউটপুট হবে সর্বনিম্ন সংখ্যাটি।
+
+সীমাবদ্ধতা
+-2 31 ≤ |S| ≤ 231 - 1
+উদাহরণ:
+Enter three numbers
+
+ইনপুট:
+10 20 30
+আউটপুট:
+10
+
+Minimal yet Important
+
+বাং
+Problem Statement
+Write a program that finds the minimum of three numbers entered by the user.
+
+Input
+The input consists of 3 integers.
+
+Output
+Output the minimum.
+
+Constraints
+-2 31 ≤ |S| ≤ 231 - 1
+Example:
+Enter three numbers
+
+Input:
+10 20 30
+Output:
+10
+
+*/
+
+
+
+
+
+
+
+
+
+
+
